1bee10fc33287c53d921e647bd2a792036df588a
leoFagundes <leofagundes2015@gmail.com>
2024-06-15 08:42:05 -0300

    feat:  printController

--- src/controller/printerController.js
+++ src/controller/printerController.js
@@ -4,12 +4,22 @@ class PrinterController {
   static async print(req, res) {
     try {
       const { text } = req.body;
-      printer.println(text);
-      printer.cut();
-      await printer.execute();
+      let isConnected = await printer.isPrinterConnected();
+
+      if (!isConnected) {
+        return res.status(500).json({ message: "Printer not connected" });
+      }
+
+      printer.alignCenter(); // Alinha o texto ao centro
+      printer.println("Hello World"); // Adiciona texto com nova linha
+      printer.println(text); // Adiciona o texto enviado no corpo da requisição
+      printer.cut(); // Corta o papel
+
+      await printer.execute(); // Executa o comando de impressão
+
       res.status(200).send({ message: "Printing completed successfully" });
     } catch (error) {
-      res.status(500).json({ message: `failed to printer - ${error}` });
+      res.status(500).json({ message: `Failed to print - ${error}` });
     }
   }
 
--- src/services/printer.js
+++ src/services/printer.js
@@ -3,14 +3,23 @@ import {
   ThermalPrinter,
   PrinterTypes,
   CharacterSet,
+  BreakLine,
 } from "node-thermal-printer";
+
+/*
+tcp://192.168.0.99:9100 -	Network printer with port
+printer:auto -Auto select raw system printer via Printer or Electron printer module
+printer:My Printer Name -	Select system printer by name via Printer or Electron printer module
+\\.\COM1 -	Print via local port or file
+ */
 
 let printer = new ThermalPrinter({
   type: PrinterTypes.EPSON,
-  interface: "tcp://192.168.11.200",
+  interface: "\\\\.\\COM1",
   characterSet: CharacterSet.PC852_LATIN2,
   removeSpecialCharacters: false,
   lineCharacter: "=",
+  breakLine: BreakLine.WORD,
   options: {
     timeout: 5000,
   },
