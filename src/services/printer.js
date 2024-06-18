6e43281713d0b7a2cdd199d21494dc169617be91
Leonardo Fagundes <89270130+leoFagundes@users.noreply.github.com>
2024-06-18 21:48:20 +0000

    test:test

--- src/services/printer.js
+++ src/services/printer.js
@@ -7,7 +7,7 @@ import {
 
 const printer = new ThermalPrinter({
   type: PrinterTypes.EPSON,
-  interface: "printer:thermalprinter", // Endereço da impressora
+  interface: "tcp://192.168.11.200", // Endereço da impressora
   characterSet: CharacterSet.PC852_LATIN2,
   options: {
     timeout: 5000, // Tempo limite de conexão (ms)
