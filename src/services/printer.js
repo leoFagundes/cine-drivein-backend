7a62262523e6d21eab5e664a396ddc23d23c0baa
Leonardo Fagundes <89270130+leoFagundes@users.noreply.github.com>
2024-06-18 21:35:12 +0000

    test:test

--- src/services/printer.js
+++ src/services/printer.js
@@ -1,46 +1,25 @@
-// Configuração do driver
-import { Printer, Types } from "node-thermal-printer";
+// printer.js
+import {
+  ThermalPrinter,
+  PrinterTypes,
+  CharacterSet,
+  BreakLine,
+} from "node-thermal-printer";
 
-Printer.init({
-  type: Types.EPSON,
-  interface: "printer:auto", // Exemplo de interface automática, ajuste conforme necessário
+/*
+tcp://192.168.0.99:9100 -	Network printer with port
+printer:auto -Auto select raw system printer via Printer or Electron printer module
+printer:My Printer Name -	Select system printer by name via Printer or Electron printer module
+\\.\COM1 -	Print via local port or file
+ */
+
+const printer = new ThermalPrinter({
+  type: PrinterTypes.EPSON,
+  interface: "tcp://192.168.11.200", // Endereço da impressora
+  characterSet: CharacterSet.PC852_LATIN2,
   options: {
-    timeout: 5000, // Tempo de timeout para comunicação com a impressora
+    timeout: 5000, // Tempo limite de conexão (ms)
   },
 });
 
-// Criação da instância da impressora
-const printer = new Printer(Printer.adapter);
-
 export default printer;
-
-// // printer.js
-// import {
-//   ThermalPrinter,
-//   PrinterTypes,
-//   CharacterSet,
-//   BreakLine,
-//   Printer,
-//   Types,
-// } from "node-thermal-printer";
-
-// /*
-// tcp://192.168.0.99:9100 -	Network printer with port
-// printer:auto -Auto select raw system printer via Printer or Electron printer module
-// printer:My Printer Name -	Select system printer by name via Printer or Electron printer module
-// \\.\COM1 -	Print via local port or file
-//  */
-
-// let printer = new ThermalPrinter({
-//   type: PrinterTypes.EPSON,
-//   interface: "printer:auto",
-//   characterSet: CharacterSet.PC852_LATIN2,
-//   removeSpecialCharacters: false,
-//   lineCharacter: "=",
-//   breakLine: BreakLine.WORD,
-//   options: {
-//     timeout: 5000,
-//   },
-// });
-
-// export default printer;
