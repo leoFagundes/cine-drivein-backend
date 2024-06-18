e55b972932f3460052e08ed48c1a31b6953759bd
Leonardo Fagundes <89270130+leoFagundes@users.noreply.github.com>
2024-06-18 21:44:00 +0000

    test:test

--- src/services/printer.js
+++ src/services/printer.js
@@ -3,19 +3,11 @@ import {
   ThermalPrinter,
   PrinterTypes,
   CharacterSet,
-  BreakLine,
 } from "node-thermal-printer";
-
-/*
-tcp://192.168.0.99:9100 -	Network printer with port
-printer:auto -Auto select raw system printer via Printer or Electron printer module
-printer:My Printer Name -	Select system printer by name via Printer or Electron printer module
-\\.\COM1 -	Print via local port or file
- */
 
 const printer = new ThermalPrinter({
   type: PrinterTypes.EPSON,
-  interface: "printer:auto", // Endereço da impressora
+  interface: "printer:thermalprinter", // Endereço da impressora
   characterSet: CharacterSet.PC852_LATIN2,
   options: {
     timeout: 5000, // Tempo limite de conexão (ms)
