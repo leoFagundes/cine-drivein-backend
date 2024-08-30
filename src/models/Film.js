93195f983e18a9ee8be1e53cc4d3355a4de97d2d
leoFagundes <leofagundes2015@gmail.com>
2024-08-30 07:38:40 -0300

    update model

--- src/models/Film.js
+++ src/models/Film.js
@@ -4,20 +4,20 @@ import mongoose from "mongoose";
 const filmSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId }, // ID do filme
-    title: { type: String, required: true }, // Título do filme
-    showtime: { type: String, required: true }, // Horário de exibição
-    image: { type: String, required: true }, // URL da imagem do filme
-    classification: { type: String, required: true }, // Classificação indicativa
-    synopsis: { type: String, required: true }, // Sinopse do filme
-    director: { type: String, required: true }, // Nome do diretor
-    writer: { type: [String], required: true }, // Nome do roteirista
-    cast: { type: [String], required: true }, // Elenco do filme
-    genres: { type: [String], required: true }, // Gêneros do filme
-    duration: { type: String, required: true }, // Duração do filme
-    language: { type: String, required: true }, // Idioma do filme
-    displayDate: { type: String, required: true }, // Data de exibição
-    trailer: { type: String, required: true }, // URL do trailer do filme
-    screening: { type: String, required: true }, // Sessão
+    title: { type: String }, // Título do filme
+    showtime: { type: String }, // Horário de exibição
+    image: { type: String }, // URL da imagem do filme
+    classification: { type: String }, // Classificação indicativa
+    synopsis: { type: String }, // Sinopse do filme
+    director: { type: String }, // Nome do diretor
+    writer: { type: [String] }, // Nome do roteirista
+    cast: { type: [String] }, // Elenco do filme
+    genres: { type: [String] }, // Gêneros do filme
+    duration: { type: String }, // Duração do filme
+    language: { type: String }, // Idioma do filme
+    displayDate: { type: String }, // Data de exibição
+    trailer: { type: String }, // URL do trailer do filme
+    screening: { type: String }, // Sessão
   },
   {
     versionKey: false, // Desabilita a versão do documento
