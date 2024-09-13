a3c9e5c0623b369a17200730bb522b78350100f8
leoFagundes <leofagundes2015@gmail.com>
2024-09-13 07:04:03 -0300

    update routes

--- src/routes/siteConfigsRouter.js
+++ src/routes/siteConfigsRouter.js
@@ -1,11 +1,21 @@
 import express from "express";
 import SiteConfigsController from "../controller/siteConfigsController.js";
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const routes = express.Router();
 
 routes.get("/site-configs", SiteConfigsController.getConfigs);
 routes.get("/site-configs/:id", SiteConfigsController.getConfigById);
 routes.post("/site-configs", SiteConfigsController.createConfig);
+routes.post(
+  "/site-configs/image",
+  upload.single("image"),
+  SiteConfigsController.createImageItem
+);
+routes.post("/items/deleteImage", SiteConfigsController.deleteItemImage);
 routes.put("/site-configs/:id", SiteConfigsController.updateConfig);
 routes.delete("/site-configs/:id", SiteConfigsController.deleteConfig);
 
